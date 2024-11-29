e111fd232e018f6e08a6db3071c9950e5386b919
gambarestudios <gambarestudios@gmail.com>
2024-11-29 21:45:08 +0300

    y

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,7 @@
 import { Stack } from "expo-router";
 import React from "react";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
