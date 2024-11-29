b6547d532081a7ad9963fdbc2c437a8f41cfc461
gambarestudios <gambarestudios@gmail.com>
2024-11-29 21:30:34 +0300

    initial commit for sketch app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,272 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { View, Dimensions, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import {
+  Canvas,
+  Circle,
+  Path,
+  Skia,
+  ImageSVG,
+} from "@shopify/react-native-skia";
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
+import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+interface IPath {
+  segments: String[];
+  color?: string;
+}
 
-export default function HomeScreen() {
+interface ICircle {
+  x: number;
+  y: number;
+}
+interface IStamp {
+  x: number;
+  y: number;
+  color: string;
+}
+
+enum Tools {
+  Pencil,
+  Stamp,
+}
+
+export default function App() {
+  const { width, height } = Dimensions.get("window");
+
+  const paletteColors = ["red", "green", "blue", "yellow"];
+
+  const svgStar =
+    '<svg class="star-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink" viewBox="0 0 200 200"><polygon id="star" fill="{{fillColor}}" points="100,0,129.38926261462365,59.54915028125263,195.10565162951536,69.09830056250526,147.55282581475768,115.45084971874736,158.77852522924732,180.90169943749473,100,150,41.2214747707527,180.90169943749476,52.447174185242325,115.45084971874738,4.894348370484636,69.09830056250527,70.61073738537632,59.549150281252636"></polygon></svg>';
+
+  const [activePaletteColorIndex, setActivePaletteColorIndex] = useState(0);
+  const [activeTool, setActiveTool] = useState<Tools>(Tools.Pencil);
+  const [paths, setPaths] = useState<IPath[]>([]);
+  const [circles, setCircles] = useState<ICircle[]>([]);
+  const [stamps, setStamps] = useState<IStamp[]>([]);
+
+  // When Reanimated is installed, Gesture Handler will try to run on the UI thread
+  // We can't do that here because we're accessing the component state, so we need set runOnJS(true)
+  const pan = Gesture.Pan()
+    .runOnJS(true)
+    .onStart((g) => {
+      if (activeTool === Tools.Pencil) {
+        // Create a new path for every new gesture start
+        const newPath: IPath = {
+          segments: [`M ${g.x} ${g.y}`],
+          color: paletteColors[activePaletteColorIndex],
+        };
+        setPaths((prevPaths) => [...prevPaths, newPath]);
+      }
+    })
+    .onUpdate((g) => {
+      if (activeTool === Tools.Pencil) {
+        setPaths((prevPaths) => {
+          // Update only the last path in the array
+          const newPaths = [...prevPaths];
+          const lastIndex = newPaths.length - 1;
+          if (newPaths[lastIndex]?.segments) {
+            newPaths[lastIndex].segments.push(`L ${g.x} ${g.y}`);
+          }
+          return newPaths;
+        });
+      }
+    })
+    .minDistance(1);
+
+  const tap = Gesture.Tap()
+    .runOnJS(true)
+    .onStart((g) => {
+      if (activeTool === Tools.Stamp) {
+        setStamps([
+          ...stamps,
+          {
+            x: g.x - 25,
+            y: g.y - 25,
+            color: paletteColors[activePaletteColorIndex],
+          },
+        ]);
+      }
+    });
+
+  const clearCanvas = () => {
+    setPaths([]);
+    setCircles([]);
+    setStamps([]);
+  };
+
+  const paletteVisible = useSharedValue(false);
+  const animatedPaletteStyle = useAnimatedStyle(() => {
+    return {
+      top: withSpring(paletteVisible.value ? -275 : -100),
+      height: withTiming(paletteVisible.value ? 200 : 50),
+      opacity: withTiming(paletteVisible.value ? 100 : 0, { duration: 100 }),
+    };
+  });
+
+  const animatedSwatchStyle = useAnimatedStyle(() => {
+    return {
+      top: withSpring(paletteVisible.value ? -50 : 0),
+      height: paletteVisible.value ? 0 : 50,
+      opacity: withTiming(paletteVisible.value ? 0 : 100, { duration: 100 }),
+    };
+  });
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <>
+      <GestureHandlerRootView>
+        <View style={{ height, width }}>
+          <GestureDetector gesture={tap}>
+            <GestureDetector gesture={pan}>
+              <Canvas style={{ flex: 8 }}>
+                {circles.map((c, index) => (
+                  <Circle key={index} cx={c.x} cy={c.y} r={10} />
+                ))}
+                {paths.map((p, index) => (
+                  <Path
+                    key={index}
+                    path={p.segments.join(" ")}
+                    strokeWidth={5}
+                    style="stroke"
+                    color={p.color}
+                  />
+                ))}
+                {stamps.map((s, index) => {
+                  const image = Skia.SVG.MakeFromString(
+                    svgStar.replace("{{fillColor}}", s.color)
+                  );
+                  if (!image) return null;
+                  return (
+                    <ImageSVG
+                      key={index}
+                      width={50}
+                      height={50}
+                      x={s.x}
+                      y={s.y}
+                      svg={image}
+                    />
+                  );
+                })}
+              </Canvas>
+            </GestureDetector>
+          </GestureDetector>
+          <View style={{ padding: 10, flex: 1, backgroundColor: "#edede9" }}>
+            <View style={{ flex: 1, flexDirection: "row" }}>
+              <Animated.View
+                style={[
+                  { padding: 10, position: "absolute", width: 60 },
+                  animatedPaletteStyle,
+                ]}
+              >
+                {paletteColors.map((c, i) => (
+                  <TouchableOpacity
+                    key={i}
+                    onPress={() => {
+                      setActivePaletteColorIndex(i);
+                      paletteVisible.value = false;
+                    }}
+                  >
+                    <View
+                      style={[
+                        {
+                          backgroundColor: c,
+                        },
+                        styles.paletteColor,
+                      ]}
+                    ></View>
+                  </TouchableOpacity>
+                ))}
+              </Animated.View>
+              <View style={styles.swatchContainer}>
+                <TouchableOpacity
+                  onPress={() => {
+                    paletteVisible.value !== true
+                      ? (paletteVisible.value = true)
+                      : (paletteVisible.value = false);
+                  }}
+                >
+                  <Animated.View
+                    style={[
+                      {
+                        backgroundColor: paletteColors[activePaletteColorIndex],
+                      },
+                      styles.swatch,
+                      animatedSwatchStyle,
+                    ]}
+                  />
+                </TouchableOpacity>
+                <View>
+                  {activeTool === Tools.Pencil ? (
+                    <TouchableOpacity
+                      onPress={() => setActiveTool(Tools.Stamp)}
+                    >
+                      <FontAwesome5
+                        name="pencil-alt"
+                        style={styles.icon}
+                      ></FontAwesome5>
+                    </TouchableOpacity>
+                  ) : (
+                    <TouchableOpacity
+                      onPress={() => setActiveTool(Tools.Pencil)}
+                    >
+                      <FontAwesome5
+                        name="stamp"
+                        style={styles.icon}
+                      ></FontAwesome5>
+                    </TouchableOpacity>
+                  )}
+                </View>
+                <TouchableOpacity onPress={clearCanvas}>
+                  <Ionicons
+                    name="trash-bin-outline"
+                    style={styles.icon}
+                  ></Ionicons>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </GestureHandlerRootView>
+      <StatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  icon: {
+    fontSize: 40,
+    textAlign: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  paletteColor: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginVertical: 5,
+    zIndex: 2,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  swatch: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderColor: "black",
+    marginVertical: 5,
+    zIndex: 1,
+  },
+  swatchContainer: {
+    flexDirection: "row",
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
